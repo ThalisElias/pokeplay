5d96b9399709b4d645414cfd19ee33c28d1bb73b
Thalis Elias <thalliselias@hotmail.com>
2024-11-05 20:41:04 -0300

    main pagina home

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function abrirMenu() {
 }
 
 toggle_btn.addEventListener("click", abrirMenu);
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+function proximaImagem() {
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
+}
+
+setInterval(function () {
+  proximaImagem();
+}, 3500);
