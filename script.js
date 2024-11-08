0f26a55b12888d711ea337a9193731bc16b86b96
Thalis Elias <thalliselias@hotmail.com>
2024-11-08 11:06:15 -0300

    reformulação do header e nav

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-const toggle_btn = document.getElementById("toggle_btn");
-const toggle_btn_Icon = document.querySelector(".toggle_btn i");
-const dropdown_header = document.querySelector(".dropdown_header");
+const showMenu = (toggleID, navID) => {
+  const toggle = document.getElementById(toggleID);
+  nav = document.getElementById(navID);
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show-menu");
 
-function abrirMenu() {
-  dropdown_header.classList.toggle("open");
-  const isOpen = dropdown_header.classList.contains("open");
-  toggle_btn_Icon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-}
+    toggle.classList.toggle("show-icon");
+  });
+};
 
-toggle_btn.addEventListener("click", abrirMenu);
+showMenu("nav-toggle", "nav-menu");
 
-// let count = 1;
-// document.getElementById("radio1").checked = true;
+const dropdownItems = document.querySelectorAll(".dropdown__item");
 
-// function proximaImagem() {
-//   count++;
-//   if (count > 3) {
-//     count = 1;
-//   }
-//   document.getElementById("radio" + count).checked = true;
-// }
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector(".dropdown__button");
+  dropdownButton.addEventListener("click", () => {
+    const showDropdown = document.querySelector(".show-dropdown");
+    toggleItem(item);
 
-// setInterval(function () {
-//   proximaImagem();
-// }, 3500);
+    if (showDropdown && showDropdown != item) {
+      toggleItem(showDropdown);
+    }
+  });
+});
+const toggleItem = (item) => {
+  const dropdownContainer = item.querySelector(".dropdown__container");
+  if (item.classList.contains("show-dropdown")) {
+    dropdownContainer.removeAttribute("style");
+    item.classList.remove("show-dropdown");
+  } else {
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+    item.classList.add("show-dropdown");
+  }
+};
